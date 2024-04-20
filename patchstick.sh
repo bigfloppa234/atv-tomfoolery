9d2ced4ebbb51cd196e29e1ec32e1b85c375d446
bigfloppa234 <104149053+bigfloppa234@users.noreply.github.com>
2024-04-19 20:33:18 -0500

    Create patchstick.sh

---
+++ patchstick.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+exec 2>/dev/console
+exec 1>/dev/console
+ 
+echo "        --- Debian 12 Patchstick Mod ---"
+ 
+echo "        * Initializing"
+mount /dev/sdc /mnt
+kexec --load /mnt/install.386/vmlinuz --initrd=/mnt/install.386/initrd.gz --command-line="video=vesafb" 
+
+echo "        * Booting to Debian"
+kexec -e
+
+echo "		  * If you see this message, something went wrong. Make sure you have a Debian 12 (i386/32-bit) installer on a USB drive."
+echo "		  * You can also open an issue at https://github.com/bigfloppa234/atv-tomfoolery/issues"
